9bf9f7baeade131ad77bb1953ab9a556a51b305e
SohumSKS <37936322+SohumSKS@users.noreply.github.com>
2022-11-09 17:29:41 -0600

    main

---
+++ main/main.ino
@@ -0,0 +1,39 @@
+int fsrPin = 0;
+int ledPin = 1;
+float fsrReading;
+int ledLevel;
+float fsrForce;
+float fsrVoltage;
+float fsrResistance;
+float fsrConductance;
+
+void setup(void) {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop(void) {
+  fsrReading = analogRead(fsrPin);
+  fsrVoltage = map(fsrReading, 0, 1023, 0, 5000);
+  // Serial.print("mV = ");
+  // Serial.println(fsrVoltage);
+  // Serial.print(",");
+
+
+
+  // Vout = Vcc * R / (R + FSR) R = 10K, Vcc = 5V
+  fsrResistance = 5000 - fsrVoltage;
+  fsrResistance *= 10000;
+  fsrResistance /= fsrVoltage;
+
+  fsrConductance = 1000000;
+  fsrConductance /= fsrResistance;
+
+  // Ffsr = ((Vcc - V) * R) / V
+  fsrForce = fsrConductance / 80;
+  //Serial.print("Force in Newtons: ");
+  Serial.println(fsrForce);
+  ledLevel = map(fsrReading, 0, 1023, 0, 255);
+  analogWrite(ledPin, ledLevel);
+  delay(200);
+}
