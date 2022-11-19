ac20fbdb6f3ef91d42bd29e326c5d126aebdc50d
SohumSKS <37936322+SohumSKS@users.noreply.github.com>
2022-11-19 00:15:10 -0600

    Update main.ino
    oops

--- main/main.ino
+++ main/main.ino
@@ -2,15 +2,17 @@ int fsrPin = 0;
 int ledR = 7;
 int ledG = 4;
 int ledB = 2;
-float fsrReading;
 int ledLevel;
 float fsrForce;
 float fsrVoltage;
 float fsrResistance;
 float fsrConductance;
-float thres1 = 3000; 
-float thres2 = 2000;
-float thres3 = 1000;
+float botGreen;
+float topGreen;
+float botRed;
+float topRed;
+float botBlue;
+float topBlue;
 
 void setup(void) {
   Serial.begin(9600);
@@ -21,8 +23,7 @@ void setup(void) {
 }
 
 void loop(void) {
-  fsrReading = analogRead(fsrPin);
-  fsrVoltage = map(fsrReading, 0, 1023, 0, 5000);
+  fsrVoltage = getVoltage();
   // Serial.print("mV = ");
   // Serial.println(fsrVoltage);
   // Serial.print(",");
@@ -31,36 +32,45 @@ void loop(void) {
   if (fsrVoltage == 0) {
     Serial.println("No pressure");
   } else {
-    if (fsrVoltage >= thres1) {
-      digitalWrite(ledR, HIGH);
-      digitalWrite(ledG, LOW);
-      digitalWrite(ledB, LOW);
-    } else if (fsrVoltage >= thres2) {
-      digitalWrite(ledG, HIGH);
-      digitalWrite(ledR, LOW);
-      digitalWrite(ledB, LOW);
-    } else if (fsrVoltage >= thres3) {
-      digitalWrite(ledB, HIGH);
-      digitalWrite(ledR, LOW);
-      digitalWrite(ledG, LOW);
-    } else {
-      digitalWrite(ledR, LOW);
-      digitalWrite(ledG, LOW);
-      digitalWrite(ledB, LOW);
-    }
     // Vout = Vcc * R / (R + FSR) R = 10K, Vcc = 5V
-    fsrResistance = 5000 - fsrVoltage;
-    fsrResistance *= 10000;
-    fsrResistance /= fsrVoltage;
-
-    fsrConductance = 1000000;
-    fsrConductance /= fsrResistance;
-
+  
     // Ffsr = ((Vcc - V) * R) / V
-    fsrForce = fsrConductance / 80;
+    fsrForce = getNewtons(fsrVoltage);
     //Serial.print("Force in Newtons: ");
     //Serial.println(fsrForce);
     Serial.println(fsrVoltage);
+    Serial.println(fsrForce);
   }
   delay(200);
+}
+void ledstuff(float volt){
+  digitalWrite(ledR, LOW);
+  digitalWrite(ledG, LOW);
+  digitalWrite(ledB, LOW);
+  if (volt >= botRed && volt <= topRed) {
+        digitalWrite(ledR, HIGH);
+  }
+  if (volt >= botGreen && volt <= topGreen) {
+        digitalWrite(ledG, HIGH);
+  }
+  if (volt >= botBlue && volt <= topBlue) {
+        digitalWrite(ledB, HIGH);
+  }
+
+}
+float getVoltage(){
+  int analogVolts = 0;
+  int numSamples = 15;
+  for(int i = 0; i<numSamples; i++){
+      analogVolts = analogVolts + analogRead(fsrPin);
+      delay(50);
+  }
+  analogVolts = analogVolts/numSamples;
+  return map(analogVolts, 0, 1023, 0, 5000);
+
+}
+float getNewtons(float voltage){
+  fsrResistance = 1000 * voltage/(5000-voltage);
+  fsrConductance = 1/fsrResistance;
+  return fsrConductance*fsrConductance;
 }
